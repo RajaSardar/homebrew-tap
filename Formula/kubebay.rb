c0c91aba5eba903aac8884f39e61ab29f44747ad
rajasardar <rj.raj@outlook.com>
2026-08-24 22:36:39 +0530

    fix: v0.1.1 checksums

--- Formula/kubebay.rb
+++ Formula/kubebay.rb
@@ -6,15 +6,15 @@ class Kubebay < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/RajaSardar/kubebay/releases/download/v#{version}/kubebay-darwin-arm64.tar.gz"
-      sha256 "bff4c3a17ade7ae41adb0d96abac6bd1c723b55df51d91971bea72d2c47ccfd8"
+      sha256 "277c96db6a0347cae9cc1a879c9a9808cf654fc31860dead3e0d0474e13fa9d7"
     else
       url "https://github.com/RajaSardar/kubebay/releases/download/v#{version}/kubebay-darwin-amd64.tar.gz"
-      sha256 "a24b7ce1e629c599f18c1c7a3d1764a8867d9ce41789a725edcef729bc613270"
+      sha256 "5f34b67c38efac69e8a0e9761e71a0755d81f9da5788411b8099713b319993c8"
     end
   end
   on_linux do
     url "https://github.com/RajaSardar/kubebay/releases/download/v#{version}/kubebay-linux-#{Hardware::CPU.arm? ? "arm64" : "amd64"}.tar.gz"
-    sha256 "f4e7a2aae40d39460467388b255dffd4d0a15d1ddb39d2656a92219016f98aa8"
+    sha256 "19839eef100c50cbcb46d77cb5c9d1fe109e6b7d841b7003ca556b09b9ae385b"
   end
 
   def install

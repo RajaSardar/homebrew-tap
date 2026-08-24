dc7b7b8d03cd64e30e64e1842baa62178c7452ab
rajasardar <rj.raj@outlook.com>
2026-08-24 20:28:28 +0530

    Add wowow cask v0.3.0 (agentic browser)

---
+++ Casks/wowow.rb
@@ -0,0 +1,32 @@
+cask "wowow" do
+  version "0.3.0"
+  sha256 arm:   "4e9bbaa3e23f7e954490d9cb5393868db41126971c19bcf3be45b59cb5998e28",
+         intel: "568fa17761b71c24a48fbb5df3b278c7cceb9b7dc61ccb811e63463cb30b9565"
+
+  on_arm do
+    url "https://github.com/RajaSardar/wowow-browser/releases/download/v#{version}/Wowow-Browser-#{version}-arm64.dmg"
+  end
+  on_intel do
+    url "https://github.com/RajaSardar/wowow-browser/releases/download/v#{version}/Wowow-Browser-#{version}.dmg"
+  end
+
+  name "Wowow Browser"
+  desc "Open-source agentic browser with a built-in AI agent that can see and operate the web"
+  homepage "https://github.com/RajaSardar/wowow-browser"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates false
+  depends_on mac: ">= :catalina"
+
+  app "Wowow Browser.app"
+
+  zap trash: [
+    "~/Library/Application Support/wowow-browser",
+    "~/Library/Preferences/dev.wowow.browser.plist",
+    "~/Library/Saved Application State/dev.wowow.browser.savedState",
+  ]
+end

130ca9ca77cdfbf7ce8f07584c4fc58d49794560
rajasardar <rj.raj@outlook.com>
2026-08-24 22:49:54 +0530

    kubebay 0.1.2

--- Formula/kubebay.rb
+++ Formula/kubebay.rb
@@ -1,20 +1,20 @@
 class Kubebay < Formula
   desc "Local-first Kubernetes IDE — engine + web UI in one binary"
   homepage "https://github.com/RajaSardar/kubebay"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/RajaSardar/kubebay/releases/download/v#{version}/kubebay-darwin-arm64.tar.gz"
-      sha256 "277c96db6a0347cae9cc1a879c9a9808cf654fc31860dead3e0d0474e13fa9d7"
+      sha256 "5ea7dc54fb53e53865e807cc6ab2c22b05faec9c56b8e5d4f9514a69823543fd"
     else
       url "https://github.com/RajaSardar/kubebay/releases/download/v#{version}/kubebay-darwin-amd64.tar.gz"
-      sha256 "5f34b67c38efac69e8a0e9761e71a0755d81f9da5788411b8099713b319993c8"
+      sha256 "887bbc477811e323622c5fe71876945b1ad09e2d35f2bbd1bb92f3f6cb798abd"
     end
   end
   on_linux do
     url "https://github.com/RajaSardar/kubebay/releases/download/v#{version}/kubebay-linux-#{Hardware::CPU.arm? ? "arm64" : "amd64"}.tar.gz"
-    sha256 "19839eef100c50cbcb46d77cb5c9d1fe109e6b7d841b7003ca556b09b9ae385b"
+    sha256 "2104b1af420b03cfc034a12386e82e6b0c963b3d11ce11b1259ae83b5e19cc7d"
   end
 
   def install
